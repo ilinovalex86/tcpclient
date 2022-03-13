51a0f113bb83531c98083cdf9c8953b33afec6d0
Alexej <ilinov86@yandex.ru>
2022-03-13 18:46:12 +0300

    rm err

--- client.go
+++ client.go
@@ -1,30 +1,45 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	cn "github.com/ilinovalex86/connection"
 	ex "github.com/ilinovalex86/explorer"
+	ie "github.com/ilinovalex86/inputevent"
 	"github.com/ilinovalex86/screenshot"
-	"image/png"
+	"image/jpeg"
+	"log"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
+const version = "0.1.1"
+const key = "2665039024792361"
+
 //Адрес и порт сервера
-var tcpServer = "ipAddress:port"
-var streamServer = "ipAddress:port"
+var tcpServer = "192.168.1.2:25001"
+var streamServer = "192.168.1.2:25002"
 
 //Структура клиента
 type clientData struct {
-	system   string
 	Sep      string
-	User     string
 	BasePath string
 	conn     net.Conn
 	Id       string
+	Version  string
+	System   string
+}
+
+type stream struct {
+	sync.Mutex
+	statusChan chan bool
+	imgChan    chan []byte
+	conn       net.Conn
 }
 
 //Глобальные ошибки
@@ -32,13 +47,30 @@ var stopErrors = []string{
 	"createId1",
 	"createId2",
 	"already exist",
+	"downloadNewClient",
 }
 
-type imgByte []byte
+// Папка для скачивания файлов
+var uploadDir = "files"
 
-func (ib *imgByte) Write(b []byte) (int, error) {
-	*ib = append(*ib, b...)
-	return len(b), nil
+func init() {
+	ie.InitIE()
+	if !ex.ExistDir(uploadDir) {
+		err := ex.MakeDir(uploadDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (cl *clientData) downloadNewClient(q cn.Query) error {
+	var err error
+	cn.SendSync(cl.conn)
+	err = cn.GetFile(q.Query, q.DataLen, cl.conn)
+	if err != nil {
+		errors.New("downloadNewClient")
+	}
+	return nil
 }
 
 //Проверяет на критическую ошибку
@@ -56,19 +88,22 @@ func newClient() *clientData {
 	fmt.Println("Инициализация")
 	id := "new client"
 	if ex.ExistFile("id.txt") {
-		id = string(ex.ReadFileFull("id.txt"))
+		idByte, err := ex.ReadFileFull("id.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		id = string(idByte)
 	}
 	cl := &clientData{
-		system:   ex.System,
 		Sep:      ex.Sep,
-		User:     ex.User,
 		BasePath: ex.BasePath,
 		Id:       id,
+		Version:  version,
+		System:   ex.System,
 	}
-	fmt.Printf("System: %9s \n", cl.system)
-	fmt.Printf("User: %10s \n", cl.User)
 	fmt.Printf("BasePath: %12s \n", cl.BasePath)
 	fmt.Printf("id: %8s \n", id)
+	fmt.Printf("Version: %8s \n", version)
 	return cl
 }
 
@@ -96,17 +131,32 @@ func (cl *clientData) connect() error {
 		}
 	case "already exist":
 		return errors.New("already exist")
+	case "downloadNewClient":
+		err = cl.downloadNewClient(q)
+		if err != nil {
+			return err
+		}
+		log.Fatal("NewClient is downloaded")
 	}
 	return nil
 }
 
 //Проходит проверку на подключение к серверу
 func (cl *clientData) validOnServer(conn net.Conn) bool {
-	s, err := cn.ReadString(conn)
+	data, err := cn.ReadBytesByLen(16, conn)
 	if err != nil {
 		return false
 	}
-	err = cn.SendString(s[len(s)/2:]+s[:len(s)/2], conn)
+	bc, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var code = make([]byte, 16)
+	bc.Decrypt(code, data)
+	s := string(code)
+	res := s[len(s)/2:] + s[:len(s)/2]
+	bc.Encrypt(code, []byte(res))
+	err = cn.SendBytes(code, conn)
 	if err != nil {
 		return false
 	}
@@ -135,18 +185,28 @@ func (cl *clientData) newId(id string) error {
 
 //Обрабатывает запрос на содержимое папки
 func (cl *clientData) dir(path string) error {
+	if path == "" {
+		path = cl.BasePath
+	}
 	if ex.ExistDir(path) {
 		res, err := ex.Explorer(path)
 		if err != nil {
-			return err
+			err = cn.SendResponse(cn.Response{Err: err}, cl.conn)
+			if err != nil {
+				return err
+			}
+			return nil
 		}
 		res["nav"] = ex.NavFunc(path)
 		data, err := json.Marshal(res)
 		if err != nil {
-			return err
+			err = cn.SendResponse(cn.Response{Err: err}, cl.conn)
+			if err != nil {
+				return err
+			}
+			return nil
 		}
-		jS := cn.Response{DataLen: len(data)}
-		err = cn.SendResponse(jS, cl.conn)
+		err = cn.SendResponse(cn.Response{DataLen: len(data)}, cl.conn)
 		if err != nil {
 			return err
 		}
@@ -156,8 +216,7 @@ func (cl *clientData) dir(path string) error {
 			return err
 		}
 	} else {
-		jS := cn.Response{Err: path + " is not exist"}
-		err := cn.SendResponse(jS, cl.conn)
+		err := cn.SendResponse(cn.Response{Err: errors.New(path + " is not exist")}, cl.conn)
 		if err != nil {
 			return err
 		}
@@ -166,11 +225,10 @@ func (cl *clientData) dir(path string) error {
 }
 
 //Обрабатывает запрос на отправку файла
-func (cl *clientData) file(path string) error {
+func (cl *clientData) fileFromClient(path string) error {
 	if ex.ExistFile(path) {
 		file, _ := os.Stat(path)
-		jS := cn.Response{DataLen: int(file.Size()), Response: file.Name()}
-		err := cn.SendResponse(jS, cl.conn)
+		err := cn.SendResponse(cn.Response{DataLen: int(file.Size()), Response: file.Name()}, cl.conn)
 		if err != nil {
 			return err
 		}
@@ -180,8 +238,7 @@ func (cl *clientData) file(path string) error {
 			return err
 		}
 	} else {
-		jS := cn.Response{Err: path + " is not exist"}
-		err := cn.SendResponse(jS, cl.conn)
+		err := cn.SendResponse(cn.Response{Err: errors.New(path + " is not exist")}, cl.conn)
 		if err != nil {
 			return err
 		}
@@ -189,56 +246,144 @@ func (cl *clientData) file(path string) error {
 	return nil
 }
 
+func (s *stream) stream() {
+	event := ""
+	var err error
+	for {
+		var events []ie.Event
+		if event == "yes" {
+			jsonEvents, err := cn.ReadByteByDelim(s.conn)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			err = json.Unmarshal(jsonEvents, &events)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		} else {
+			cn.ReadSync(s.conn)
+		}
+		ie.EventsRun(events)
+		s.statusChan <- true
+		if ok := <-s.statusChan; !ok {
+			return
+		}
+		imgB := <-s.imgChan
+		err = cn.SendResponse(cn.Response{DataLen: len(imgB)}, s.conn)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		event, err = cn.ReadString(s.conn)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		err = cn.SendBytes(imgB, s.conn)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+	s.statusChan <- false
+}
+
 //Обрабатывает запрос на отправку файла
-func (cl *clientData) stream(c chan string) {
+func (cl *clientData) stream(c chan error) {
 	streamConn, err := net.Dial("tcp", streamServer)
 	if err != nil {
-		c <- "streamServer not found"
+		c <- errors.New("streamServer not found")
 		return
 	}
 	defer streamConn.Close()
 	if !cl.validOnServer(streamConn) {
-		c <- "error valid stream server"
+		c <- errors.New("error valid stream server")
 		return
 	}
 	jsonData, err := json.Marshal(cl)
+	if err != nil {
+		c <- errors.New("json.Marshal(cl)")
+		return
+	}
 	err = cn.SendBytesWithDelim(jsonData, streamConn)
 	if err != nil {
-		c <- fmt.Sprint(err)
+		c <- err
 		return
 	}
 	con, err := screenshot.Connect()
 	if err != nil {
-		c <- fmt.Sprint(err)
+		c <- err
 		return
 	}
 	defer screenshot.Close(con)
-	c <- "ok"
+	ss := screenshot.ScreenSize(con)
+	jsonSS, err := json.Marshal(ss)
+	if err != nil {
+		c <- errors.New("json.Marshal(ss)")
+		return
+	}
+	cn.ReadSync(streamConn)
+	err = cn.SendBytesWithDelim(jsonSS, streamConn)
+	if err != nil {
+		c <- err
+		return
+	}
+	c <- nil
+	imgChan := make(chan []byte)
+	statusChan := make(chan bool)
+	s := &stream{statusChan: statusChan, imgChan: imgChan, conn: streamConn}
+	go s.stream()
 	for {
-		cn.ReadSync(streamConn)
 		img, err := screenshot.CaptureScreen(con)
 		if err != nil {
 			fmt.Println(err)
-			return
+			break
 		}
-		ib := &imgByte{}
-		err = png.Encode(ib, img)
+		buf := new(bytes.Buffer)
+		err = jpeg.Encode(buf, img, &jpeg.Options{Quality: 96})
 		if err != nil {
 			fmt.Println(err)
+			break
+		}
+		imgB := buf.Bytes()
+		if ok := <-statusChan; !ok {
+			fmt.Println("end screen1")
 			return
 		}
-		err = cn.SendResponse(cn.Response{DataLen: len(*ib)}, streamConn)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		cn.ReadSync(streamConn)
-		err = cn.SendBytes(*ib, streamConn)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		statusChan <- true
+		imgChan <- imgB
 	}
+	if ok := <-statusChan; !ok {
+		fmt.Println("end screen1")
+		return
+	}
+	statusChan <- false
+	fmt.Println("end screen2")
+}
+
+func (cl *clientData) fileToClient(q cn.Query) error {
+	switch ex.System {
+	case "linux":
+		uploadDir += "/" + q.Query
+	case "windows":
+		uploadDir += "\\" + q.Query
+	}
+	cn.SendSync(cl.conn)
+	err := cn.GetFile(uploadDir, q.DataLen, cl.conn)
+	if err != nil {
+		err = cn.SendResponse(cn.Response{Err: err}, cl.conn)
+		if err != nil {
+			return err
+		}
+		return err
+	}
+	err = cn.SendResponse(cn.Response{}, cl.conn)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 //Принимает сообщения от сервера и обрабатывает их.
@@ -248,6 +393,7 @@ func worker(cl *clientData) error {
 		if err != nil {
 			return err
 		}
+		fmt.Printf("%#v %s \n", q, time.Now().Format(time.StampNano))
 		switch q.Method {
 		case "testConnect":
 			err = cn.SendResponse(cn.Response{}, cl.conn)
@@ -259,20 +405,27 @@ func worker(cl *clientData) error {
 			if err != nil {
 				return err
 			}
-		case "file":
-			err = cl.file(q.Query)
+		case "fileFromClient":
+			err = cl.fileFromClient(q.Query)
+			if err != nil {
+				return err
+			}
+		case "fileToClient":
+			err = cl.fileToClient(q)
 			if err != nil {
 				return err
 			}
 		case "stream":
-			channel := make(chan string)
+			channel := make(chan error)
 			go cl.stream(channel)
-			r := <-channel
-			if r == "ok" {
-				err = cn.SendResponse(cn.Response{Response: r}, cl.conn)
-			} else {
-				err = cn.SendResponse(cn.Response{Err: r}, cl.conn)
+			err = <-channel
+			if err != nil {
+				err = cn.SendResponse(cn.Response{Err: err}, cl.conn)
+				if err != nil {
+					return err
+				}
 			}
+			err = cn.SendResponse(cn.Response{}, cl.conn)
 			if err != nil {
 				return err
 			}
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module tcpclient
+module client
 
 go 1.13
 
 require (
-	github.com/ilinovalex86/connection v1.1.5
-	github.com/ilinovalex86/explorer v1.1.0
-	github.com/ilinovalex86/screenshot v1.0.1
+	github.com/ilinovalex86/connection v1.2.8
+	github.com/ilinovalex86/explorer v1.2.2
+	github.com/ilinovalex86/inputevent v0.1.1
+	github.com/ilinovalex86/screenshot v1.0.4
 )
